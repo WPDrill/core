0c9e8c80b12acdb714759c0c2fc18c3e78cd8d37
Nahid Bin Azhar <nahid.dns@gmail.com>
2024-04-01 22:09:51 +0600

    added method stubs for Model

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -5,6 +5,23 @@ namespace WPDrill\Models;
 use WPDrill\DB\QueryBuilder\QueryBuilderHandler;
 use WPDrill\Facade;
 
+
+/**
+ * @method static self select(string ...$columns)
+ * @method static array|object|null get()
+ * @method static \stdClass|null first()
+ * @method static \stdClass|null findAll(string $fieldName, $value)
+ * @method static \stdClass|null find($value, $fieldName = 'id')
+ * @method static int count()
+ * @method static array|string insert(array $data)
+ * @method static array|string update(array $data)
+ * @method static array|string updateOrInsert(array $data)
+ * @method static mixed delete()
+ * @method static self where(string $key, $operator = null, $value = null)
+ * @method static self orWhere(string $key, $operator = null, $value = null)
+ * @method static self whereIn(string $key, array $values)
+ * @method static self whereNull(string $key)
+ */
 abstract class Model extends Facade
 {
     /**
