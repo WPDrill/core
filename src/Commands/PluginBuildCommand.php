af45b6ef7057564b7f5fb6243d4d10174acbdaf9
Nahid Bin Azhar <nahid.dns@gmail.com>
2024-09-17 20:10:33 +0600

    fixed build option

--- src/Commands/PluginBuildCommand.php
+++ src/Commands/PluginBuildCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableSeparator;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use WPDrill\DB\Migration\Migrator;
 use WPDrill\Facades\Config;
 
@@ -17,6 +18,7 @@ class PluginBuildCommand extends BaseCommand
     protected function configure()
     {
         $this->setName('plugin:build')
+             ->addOption('prod', 'p', null, 'Build the plugin for production, this build remove dev dependencies')
              ->setDescription('Build the plugin for production')
              ->setHelp('This build command allow you to build the plugin for production.');
     }
@@ -24,15 +26,43 @@ class PluginBuildCommand extends BaseCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $start = time();
+
+        $io = new SymfonyStyle($input, $output);
+
         $date = new DateTime("now", new DateTimeZone("UTC"));
-        $outputDir = '/.dist/build-' . $date->format("Y-m-d\TH:i:s\Z");
-        $output->writeln('<info>Building the plugin...</info>');
+        $buildName = 'build-' . $date->format("Y-m-d\TH:i:s\Z");
+        if ($input->getOption('prod')) {
+            $buildName .= '-prod';
+        }
+
+        $outputDir = WPDRILL_ROOT_PATH . '/.dist/' . $buildName;
+
+
+        $io->newLine();
+        $output->writeln('Building the plugin...');
+
+        if ($input->getOption('prod')) {
+            $this->process(['composer', 'install', '--no-dev']);
+        }
+
         $buildProcess = $this->process(['vendor/bin/php-scoper', 'add-prefix', '--force', '--output-dir=' . $outputDir]);
-        $end = time();
-        $totalTime = ($end - $start);
+
         if ($buildProcess->isSuccessful()) {
-            $output->writeln('<success>Plugin build successfully!</success>');
-            $output->writeln('<info>Time: ' . $totalTime . ' Seconds</info>');
+            if ($input->getOption('prod')) {
+                $this->process(['composer', 'install']);
+            }
+
+            $this->process(['cd', $outputDir, '&&', 'composer', 'dump-autoload']);
+            $end = time();
+            $totalTime = ($end - $start);
+
+            $io->newLine();
+            $output->writeln('<info>Plugin build successfully!</info>');
+            $io->newLine();
+            $output->writeln([
+                '<comment>Build Name: ' . $buildName. '</comment>',
+                '<comment>Time: ' . $totalTime . ' Seconds</comment>'
+            ]);
         } else {
             $output->writeln('<error>' . $buildProcess->getErrorOutput() .'</error>');
             return Command::FAILURE;
@@ -42,4 +72,3 @@ class PluginBuildCommand extends BaseCommand
         return Command::SUCCESS;
     }
 }
-
